658bcd71908dc33d55243060d08d0cb410878a14
sidhi-sid <sidhisidh469@gmail.com>
2020-06-19 21:19:01 +0530

    adding state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Board from './Board'
 
-export default function Game(){
-    return(
-        <div className="game">
-            <div className="game-board">
-            <Board/>
+export default class Game extends React.Component{
+    constructor(){
+        super() 
+        this.state={
+            xisnext:true,
+            stepno:0,
+            history:[
+                {squares: Array(9).fill(null)}
+            ]
+        }
+    }
+    render(){
+        return(
+            <div className="game">
+                <div className="game-board">
+                <Board/>
+                </div>
             </div>
-        </div>
-       
-    )
+           
+        )
+    }
+    
 }
