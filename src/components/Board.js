5d4cf68e5943aadd157398768e6dea463048292e
sidhi-sid <sidhisidh469@gmail.com>
2020-06-19 21:03:34 +0530

    implemented basic UI

---
+++ src/components/Board.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Square from './Square'
+
+export default function Board(){
+    return( //wrapping 3 rows in a single div so parent div will be returned consisting of 3 child div
+    <div>
+        <div className="border-row">
+            <Square/>  
+            <Square/>
+            <Square/>
+        </div>
+
+        <div className="border-row">
+            <Square/>  
+            <Square/>
+            <Square/>
+        </div>
+        
+        <div className="border-row">
+            <Square/>  
+            <Square/>
+            <Square/>
+        </div>
+    </div>
+    )
+}
